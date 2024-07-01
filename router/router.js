becc1e8a86ba3fea38e97250d00b8665a67a0bf3
Dr-AddictStein <dr.addictstein@gmail.com>
2024-07-01 13:00:57 +0600

    check

--- router/router.js
+++ router/router.js
@@ -61,7 +61,7 @@ router.post("/add", upload.single("image"), async (req, res) => {
 });
 
 router.post("/update/:id", async (req, res) => {
-    console.log("HERERooo",req.body)
+    console.log("HERERooo Issues",req.body)
     try {
         const id = req.params.id;
         const upData = req.body;
